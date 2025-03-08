ab657312625b287fe273007f886d8e55c20d045b
caffae <wangyeelin@gmail.com>
2025-03-08 20:40:21 +0800

    fix up release script

--- prepareForGitRelease.sh
+++ prepareForGitRelease.sh
@@ -2,21 +2,21 @@
 npm version patch
 
 # create the current_release directory if it does not exist
-mkdir -p current-folder-notes
+mkdir -p editor-to-clipboard
 
 # make a copy of the main.js, manifest.json, and styles.css files in another folder
-cp main.js current-folder-notes
-cp manifest.json current-folder-notes
-cp styles.css current-folder-notes
+cp main.js editor-to-clipboard
+cp manifest.json editor-to-clipboard
+cp styles.css editor-to-clipboard
 
 # compress the current_release folder into a zip file
 # zip -r release.zip current_release
-zip -vr current-folder-notes.zip current-folder-notes -x "*.DS_Store"
+zip -vr editor-to-clipboard.zip editor-to-clipboard -x "*.DS_Store"
 
-mv current-folder-notes.zip release.zip
+mv editor-to-clipboard.zip release.zip
 
 # remove the current_release folder
-# rm -rf current-folder-notes
+# rm -rf editor-to-clipboard
 
 git add -A
 git commit -m "Prepare for Git Release"
